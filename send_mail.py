fe2cdb90321d47a866da985b4b98c8f9c55ebd19
cjf8899 <53032349+cjf8899@users.noreply.github.com>
2021-02-07 18:35:33 +0900

    Update send_mail.py

--- send_mail.py
+++ send_mail.py
@@ -2,8 +2,10 @@
 
 # 사용방법
 # 1. mail.txt 파일의 첫 줄은 제목, 나머지는 내용으로 채워주세요.
-# 2. mail_list.csv 파일의 첫 줄은 보내는 사람(from), 나머지는 받는 사람(to) 입니다.
+# 2. mail_list.csv 파일의 첫 줄은 보내는 사람(from)과 보내는 사람 이메일, 나머지는 받는 사람(to)과 받는 사람 이메일 입니다.
 # 3. 로그인 인증시 https://yeolco.tistory.com/93 블로그를 참고하여 사용해 주세요!
+# 4. csv한글은 오른쪽 하단부 Spaces: 옆에 있는 버튼 클릭 후, Reopen with encoding 에서 korean 검색 후 설정
+# 5. 그 후, save with encoding 에서 UTF-8으로 설정
 
 ##########################################################################
 import smtplib
@@ -16,12 +18,15 @@ s = smtplib.SMTP('smtp.gmail.com', 587)
 # TLS 보안 시작
 s.starttls()
 
+
 ########## 로그인 인증##########
 
-s.login('user@gmail.com', 'token_password')
+s.login('nhchul99@gmail.com', 'dlurcpzwpiafzibo')
 
 ###############################
 
+
+# txt 파일 읽어오기
 # 메일 작성
 print("메일 내용 : \n")
 f = open("./mail.txt", 'r',encoding='UTF8')
@@ -39,28 +44,30 @@ while True:
 f.close()
 
 
-msg = MIMEText(message)
-msg['Subject'] = title
+# csv 파일 읽어오기
+# 보내는 이메일 , 받는 사람, 받는 사람 이메일
+print("받는사람 : \n")
+mail_list = []
+mail_name_list = []
+nameplus = ''
+f = open('./mail_list.csv', 'r', encoding='utf-8')
+rdr = csv.reader(f)
+for count, (name, email) in enumerate(rdr):
+    if count == 0:
+        mail_from = str(email)
+    else:
+        mail_name_list.append(name)
+        mail_list.append(email)
+        print(" name : ",name)
+        print(" email : ",email)
 
 
 # 메일 보내기
-# 보내는 이메일 , 받는 이메일
-print("받는사람 : \n")
-count = 0
-mail_list = []
-f = open('./mail_list.csv', 'r', encoding='utf-8')
-rdr = csv.reader(f)
-for line in rdr:
-    if count == 0:
-        mail_from = str(line)
-        count += 1
-    else:
-        mail_list.append(line)
-        print(line)
-f.close()  
-
-for to in range(len(mail_list)):
-    s.sendmail(mail_from, mail_list[to], msg.as_string())
+for idx in range(len(mail_name_list)):
+    nameplus = str(mail_name_list[idx]) + " 님, "+ message
+    msg = MIMEText(nameplus)
+    msg['Subject'] = title
+    s.sendmail(mail_from, mail_list[idx], msg.as_string())
 
 # 세션 종료
 s.quit()
